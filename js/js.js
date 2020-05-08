8890a503afa166cddbcb0a69a3891eb69704017b
viptiria <62742679+viptiria@users.noreply.github.com>
2020-05-09 00:41:58 +0700

    VIPtoria

--- js/js.js
+++ js/js.js
@@ -1,6 +1,12 @@
 $("a").click(function(){
   $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top - 80
+   scrollTop:$("#" + $(this).data('value')).offset().top-100
   },1500)
   
  })
+$(document).ready(function () {
+        $(".navbar-nav a").click(function (e) {
+            e.preventDefault();
+            $('.navbar-collapse.show').collapse('hide');
+        });
+    });
